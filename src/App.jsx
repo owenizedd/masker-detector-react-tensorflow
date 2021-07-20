6706c993858ba4fa1c142de18ac6a832af583e48
Ryan Owen Thionanda <owenizedd@gmail.com>
2021-07-20 17:52:48 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef, useEffect } from 'react'
+import './App.css'
+import * as blazeface from '@tensorflow-models/blazeface';
+import * as tf from '@tensorflow/tfjs';
+import { detectMaskByColors, drawRectangle } from './utils/drawer';
+
+import Webcam from 'react-webcam'
+function App() {
+  const webcamRef = useRef();
+  const canvasRef = useRef();
+  const canvas2Ref = useRef();
+  const model = useRef();
+
+  const detectMask = async () => {
+    if (webcamRef.current &&  webcamRef.current.video.readyState === 4){
+      canvasRef.current.width = webcamRef.current.video.videoWidth;
+      canvasRef.current.height = webcamRef.current.video.videoHeight;
+      canvas2Ref.current.width = canvasRef.current.width;
+      canvas2Ref.current.height = canvasRef.current.height;
+      const predictions = await model.current.estimateFaces(webcamRef.current.video, false);
+      if (predictions.length) {
+        const ctx2 = canvas2Ref.current.getContext('2d');
+        const isUsingMasks = detectMaskByColors(predictions, webcamRef.current.video, ctx2);
+        const ctx = canvasRef.current.getContext('2d');
+
+        drawRectangle(predictions, ctx, isUsingMasks);  
+
+        
+      }
+    }
+  }
+  useEffect(async () => {
+    model.current = await blazeface.load();
+    setInterval(() => {
+      detectMask();
+    }, 75);
+
+  }, [])
+  return (
+    <div className="App">
+      <div className="webcam-ai__container">
+        <Webcam ref={webcamRef} className="webcam" width={1000}/>
+        <canvas className="webcam-ai__canvas" ref={canvasRef}/>
+        <canvas className="webcam-color__canvas" ref={canvas2Ref}/>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/drawer.js
@@ -0,0 +1,79 @@
+export const drawRectangle = (predictions, ctx, isUsingMasks) => {
+  predictions.forEach( (prediction, index) => {
+    const start = prediction.topLeft;
+    const end = prediction.bottomRight;
+
+
+    const topLeft = [start[0], start[1]];
+    const topRight = [end[0], start[1]];
+    const bottomRight = [end[0], end[1]];
+    const bottomLeft = [start[0], end[1]];
+    
+    const rects = [
+      topLeft,
+      topRight,
+      bottomRight,
+      bottomLeft,
+      topLeft,
+    ]
+    // Render a rectangle over each detected face.
+    for(let i = 0; i < rects.length - 1; i++){
+      ctx.beginPath();
+      ctx.strokeStyle='green';
+      ctx.lineWidth=4;
+      ctx.moveTo(rects[i][0], rects[i][1]);
+      ctx.lineTo(rects[i+1][0], rects[i+1][1]);
+      ctx.stroke();
+    }
+
+    for(let j = 0; j < prediction.landmarks.length; j++){
+      //get x and y
+      const x = prediction.landmarks[j][0];
+      const y = prediction.landmarks[j][1];
+      ctx.beginPath();
+
+      ctx.arc(x, y, 4, 0, 2 * Math.PI);
+    
+      ctx.fillStyle='red';
+      ctx.fill();
+    }
+    
+
+    ctx.font = "30px Arial";
+    if (isUsingMasks[index]){
+      ctx.fillStyle='lightgreen';
+      ctx.fillText("Kamu memakai masker", prediction.topLeft[0], prediction.topLeft[1] - 30); 
+    }
+    else{
+      ctx.fillStyle='lightred';
+      ctx.fillText("Kamu tidak memakai masker", prediction.topLeft[0], prediction.topLeft[1] - 30);
+    }
+    
+
+
+
+  });
+}
+
+
+export const detectMaskByColors = (predictions, video, ctx) => {
+  ctx.drawImage(video, 0, 0, 960 / 1.5, 720 / 1.5);
+ 
+  const results = [];
+
+  predictions.forEach(prediction => {
+    const noseColor = ctx.getImageData(prediction.landmarks[2][0], prediction.landmarks[2][1], 1, 1).data;
+    const mouthColor = ctx.getImageData(prediction.landmarks[3][0], prediction.landmarks[3][1], 1, 1).data;
+    const [nR, nG, nB] = noseColor;
+    const [mR, mG, mB] = mouthColor;
+    const diff = Math.abs(nR - mR) + Math.abs(nG - mG) + Math.abs(nB, mB);
+
+    if (diff < 110) 
+      results.push(true);
+    else results.push(false);
+  })
+
+  return results;
+
+  return results;
+}
